fa6698657e4a93a033845a818123f011bbf2e010
LeoZ213 <113804442+LeoZ213@users.noreply.github.com>
2022-11-16 21:14:07 -0500

    Update Main.java

--- part05-Part05_15.Archive/src/main/java/Main.java
+++ part05-Part05_15.Archive/src/main/java/Main.java
@@ -1,26 +1,25 @@
-
+ 
+ 
 import java.util.ArrayList;
 import java.util.Scanner;
-
+ 
+ 
+ 
 public class Main {
-
+ 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> identifier = new ArrayList<>();
         ArrayList<String> name = new ArrayList<>();
         ArrayList<String> alreadyPrinted = new ArrayList<>();
-
+ 
         while (true) {
             System.out.println("Identifier? (empty will stop)");
             String identify = scanner.nextLine();
             if (identify.isEmpty()) {
                 break;
             }
-            if (identifier.contains(identify)) {
-
-            } else {
-                identifier.add(identify);
-            }
+            identifier.add(identify);
             System.out.println("Name? (empty will stop)");
             String namex = scanner.nextLine();
             if (namex.isEmpty()) {
@@ -28,17 +27,16 @@ public class Main {
             }
             name.add(namex);
         }
-
+ 
         System.out.println("==Items==");
-        
-        for (int i = 0; i < identifier.size() && i < name.size();i++) {
+ 
+        for (int i = 0; i < identifier.size() && i < name.size(); i++) {
             if (alreadyPrinted.contains(identifier.get(i))) {
-                
+ 
             } else {
                 System.out.println(identifier.get(i) + ": " + name.get(i));
                 alreadyPrinted.add(identifier.get(i));
             }
         }
     }
-
-}
+ 
