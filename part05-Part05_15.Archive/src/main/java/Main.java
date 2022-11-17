d8209d276a9a1eb4ead2f66a2feed77268ad43a0
LeoZ213 <113804442+LeoZ213@users.noreply.github.com>
2022-11-16 21:06:58 -0500

    Add files via upload

---
+++ part05-Part05_15.Archive/src/main/java/Main.java
@@ -0,0 +1,44 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> identifier = new ArrayList<>();
+        ArrayList<String> name = new ArrayList<>();
+        ArrayList<String> alreadyPrinted = new ArrayList<>();
+
+        while (true) {
+            System.out.println("Identifier? (empty will stop)");
+            String identify = scanner.nextLine();
+            if (identify.isEmpty()) {
+                break;
+            }
+            if (identifier.contains(identify)) {
+
+            } else {
+                identifier.add(identify);
+            }
+            System.out.println("Name? (empty will stop)");
+            String namex = scanner.nextLine();
+            if (namex.isEmpty()) {
+                break;
+            }
+            name.add(namex);
+        }
+
+        System.out.println("==Items==");
+        
+        for (int i = 0; i < identifier.size() && i < name.size();i++) {
+            if (alreadyPrinted.contains(identifier.get(i))) {
+                
+            } else {
+                System.out.println(identifier.get(i) + ": " + name.get(i));
+                alreadyPrinted.add(identifier.get(i));
+            }
+        }
+    }
+
+}
